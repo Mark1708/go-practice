49fff0d1ccb5c767010e73cf22a32d00bc640af1
Mark Gurianov <mark.gurianov@gmail.com>
2024-03-02 14:20:40 +0300

    Create HLS Server

---
+++ hls/README.md
@@ -0,0 +1,28 @@
+# Simple File Server
+
+## Preparing
+### Installing FFmpeg
+```bash
+# For mac
+brew install ffmpeg
+
+# For linux
+apt install ffmpeg
+```
+### Convert MP3 and MP4
+```bash
+# Prepare mp3
+ffmpeg -i Shahmen-Mark.mp3 -c:a libmp3lame -b:a 128k -map 0:0 -f segment -segment_time 10 -segment_list outputlist.m3u8 -segment_format mpegts output%03d.ts
+
+# Prepare mp4
+ffmpeg -i Shahmen-Mark.mp4 -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls outputlist.m3u8
+```
+
+## Run apps
+```bash
+# Run server
+go run hls/main.go
+```
+
+## Resources
+ - https://habr.com/ru/companies/odnoklassniki/articles/467669/
---
+++ hls/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var (
+	port          = "8000"
+	mainPath      = "/"
+	storagePath   = "/storage/"
+	playerServer  = http.FileServer(http.Dir("hls/player"))
+	storageServer = http.FileServer(http.Dir("hls/storage"))
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:           "localhost:" + port,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	mux.Handle(mainPath, addHeaders(playerServer))
+	mux.Handle(storagePath, addHeaders(storageServer))
+
+	stopped := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+		<-sigint
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("HTTP Server Shutdown Error: %v", err)
+		}
+		close(stopped)
+	}()
+
+	fmt.Println("HLS Player App started!")
+	log.Printf("Starting HTTP server on %s", port)
+
+	if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalf("HTTP server ListenAndServe Error: %v", err)
+	}
+
+	<-stopped
+	fmt.Println("HLS Player App stopped")
+
+}
+
+// addHeaders Функция добавляет поддержку CORS
+func addHeaders(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		h.ServeHTTP(w, r)
+	}
+}
